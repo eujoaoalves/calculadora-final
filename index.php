bc647f3cadb6266d8351c670118695f49103f4f0
Joao G <joao.guylherme@hotmail.com>
2023-06-13 16:41:10 -0300

    Adicionei subtração e divisão

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
     $firstNumber = readline('First number: ');
     $secondNumber = readline('Second number: ');
-        $operation = readline('Operation: ');
+    $operation = readline('Operation: ');
+    $resultado = 0;
+
+    switch ($operation) {
+        case '-':
+            $resultado = $firstNumber - $secondNumber;
+            break;
+
+        case '/':
+            $resultado = $firstNumber / $secondNumber;
+            break;
+        
+        default:
+            $resultado = 'Digite um operador valido';
+            break;
+    }
+
+    echo $resultado;
 ?>
